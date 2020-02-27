5ce2ccff5342193bc5a84488f1ec6637590c9380
Dominic Chu <dominic.m.chu@gmail.com>
2020-02-27 12:31:42 -0500

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,11 +1,31 @@
 // Add BandInput component
-import React, { Component } from 'react'
+import React, { Component } from 'react'; 
 
 class BandInput extends Component {
+  state ={
+    name: ''
+  }
+  handleChange = event => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+  handleSubmit = event => {
+    event.preventDefault() 
+    console.log(this.state)
+    this.props.addBand(this.state)
+    this.setState({
+      name: ''
+    })
+  }
   render() {
     return(
       <div>
-        Band Input
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.name} onChange={this.handleChange} type="text"/> 
+          <input type="submit" /> 
+        </form>
+
       </div>
     )
   }
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import BandInput from '../components/BandInput'
+import manageBand from '../reducers/manageBand'
 
 class BandsContainer extends Component {
+  // new able to acceess the dispatch as this.props.manageBand(newband)
+  mapAllBands = () => {
+    return this.props.bands.map(band => {
+        return <li> {band.name} </li>
+    })
+  }
   render() {
+    console.log(this.props.addBand)
+    console.log(this.props.bands)
     return(
       <div>
-        BandsContainer
+        <BandInput addBand={this.props.addBand} />
+        <ul>
+          {this.mapAllBands()}
+        </ul> 
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addBand: (newBand) => {
+      dispatch({
+        type: 'ADD_BAND',
+        band: newBand
+      })
+    }, 
+  }
+}
+// const mapStateToProps = state => {
+//   return {
+//     bands: state.bands 
+//   }
+// }
+
+export default connect(state => ({ bands: state.bands }), mapDispatchToProps)(BandsContainer)
